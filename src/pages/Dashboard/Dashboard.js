4377f3ef6a935a592a4ddfe428151ec283d6fe6c
M-Asad-khan <asad.khan@devbox.co>
2022-06-03 19:56:47 +0500

    remove extra packages and components

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,27 +2,11 @@ import React from 'react';
 import Navigation from '../../components/Navigation/Navigation';
 import {Row, Breadcrumb, Col } from 'react-bootstrap';
 import Footer from '../Footer/Footer';
-import ThisYearSales from '../../components/Dashboard/ThisYearSales';
-import ThisMonthSales from '../../components/Dashboard/ThisMonthSales';
-import TrendingProduct from '../../components/Dashboard/TrendingProduct';
-import SalesByCountries from '../../components/Dashboard/SalesByCountries';
-import LastWeekSales from '../../components/Dashboard/LastWeekSales';
-import TopRatedProducts from '../../components/Dashboard/TopRatedProducts';
-import Tickets from '../../components/Dashboard/Tickets';
-import ProductCategories from '../../components/Dashboard/ProductCategories';
-import UserActivities from '../../components/Dashboard/UserActivities';
-import NewUsers from '../../components/Dashboard/NewUsers';
-import PaymentsDone from '../../components/Dashboard/Payments/PaymentsDone';
-import PaymentsDue from '../../components/Dashboard/Payments/PaymentsDue';
-import PaymentsWithdraw from '../../components/Dashboard/Payments/PaymentsWithdraw';
-import BestSellers from '../../components/Dashboard/BestSellers';
-import MapContainer from '../../components/MapContainer';
 import MonthlySales from '../../components/Dashboard/Sales/MonthlySales';
 import TotalOrders from '../../components/Dashboard/Sales/TotalOrders';
 import PendingOrders from '../../components/Dashboard/Sales/PendingOrders';
 import CompletedOrders from '../../components/Dashboard/Sales/CompletedOrders';
 import Loader from '../../components/Common/Loader';
-import VisitorStats from '../../components/Dashboard/VisitorStats';
 
 class Dashboard extends React.Component {
     state = {
@@ -68,7 +52,7 @@ class Dashboard extends React.Component {
                         <Breadcrumb>
                             <h1>Dashboard</h1>
                             <Breadcrumb.Item to="/dashboard">Dashboard</Breadcrumb.Item>
-                            <Breadcrumb.Item active>Dashboard v.1</Breadcrumb.Item>
+                            <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
                         </Breadcrumb>
                     </div>
                     {/* End Breadcrumb */}
@@ -95,101 +79,10 @@ class Dashboard extends React.Component {
                         </Col>
                     </Row>
 
-                    <Row>
-                        <Col lg={12}>
-                            {/* File path: src/components/Dashboard/ThisYearSales.js */}
-                            <ThisYearSales />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col lg={5}>
-                            {/* File path: src/components/Dashboard/SalesByCountries.js */}
-                            <SalesByCountries />
-                        </Col>
-
-                        <Col lg={7}>
-                            {/* File path: src/components/Dashboard/TrendingProduct.js */}
-                            <TrendingProduct />
-                        </Col>
-                        
-                        <Col lg={12}>
-                            {/* File path: src/components/Dashboard/ThisMonthSales.js */}
-                            <ThisMonthSales />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col lg={6}>
-                            {/* File path: src/components/Dashboard/LastWeekSales.js */}
-                            <LastWeekSales />
-                        </Col>
-                        <Col lg={6}>
-                            {/* File path: src/components/Dashboard/TopRatedProducts.js */}
-                            <TopRatedProducts />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col lg={6}>
-                            {/* File path: src/components/Dashboard/Tickets.js */}
-                            <Tickets />
-                        </Col>
-
-                        <Col lg={6}>
-                            {/* File path: src/components/Dashboard/ProductCategories.js */}
-                            <ProductCategories />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col lg={6}>
-                            {/* File path: src/components/Dashboard/UserActivities.js */}
-                            <UserActivities />
-                        </Col>
-
-                        <Col lg={6}>
-                            {/* File path: src/components/Dashboard/NewUsers.js */}
-                            <NewUsers />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col lg={4}>
-                            {/* File path: src/components/Deshboard/Payments/PaymentsDone.js */}
-                            <PaymentsDone />
-                        </Col>
-
-                        <Col lg={4}>
-                            {/* File path: src/components/Deshboard/Payments/PaymentsDue.js */}
-                            <PaymentsDue />
-                        </Col>
-
-                        <Col lg={4}>
-                            {/* File path: src/components/Deshboard/Payments/PaymentsWithdraw.js */}
-                            <PaymentsWithdraw />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col lg={12}>
-                            {/* File path: src/components/Dashboard/VisitorStats.js */}
-                            <VisitorStats />
-                        </Col>
-                    </Row>
                     
-                    <Row>
-                        <Col lg={6}>
-                            {/* File path: src/components/Dashboard/BestSellers.js */}
-                            <BestSellers />
-                        </Col>
-                        
-                        <Col lg={6}>
-                            {/* File path: src/components/MapContainer.js */}
-                            <MapContainer />
-                        </Col>
-                    </Row>
 
+                   
+                    
                     {/* Footer */}
                     <div className="flex-grow-1"></div>
                     <Footer /> 
