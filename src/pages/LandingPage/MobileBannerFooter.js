80000e0d727fcb470c65981d8d677daf6cbd3c46
abbasanwar158 <abbas.anwar@devbox.co>
2022-06-08 18:28:19 +0500

    mobile footer design set

---
+++ src/pages/LandingPage/MobileBannerFooter.js
@@ -0,0 +1,22 @@
+import React, { useState, useContext, useEffect } from "react";
+
+export default function MobileBannerFooter() {
+
+    return (
+      <div>
+        <div class="mobile-footer">
+          <div class="footer-bottom">
+            <div class="footer-links">
+              <a href="/">About</a>
+              <a href="/">Products</a>
+              <a href="/">Contact</a>
+              <a href="/">Privacy</a>
+              <a href="/">Terms</a>
+              <a href="/">Sitemap</a>
+            </div>
+            <p class="copy">Copyright Ⓒ 2003 - 2022 TractorOnline (Pvt) Ltd. - All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    );
+}
