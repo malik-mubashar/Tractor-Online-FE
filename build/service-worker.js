ac15714124289b6520ffeb5ac55aa411a0589da3
mubashar-hassan-awan <mubashar.hassan@devbox.co>
2022-06-09 12:15:09 +0500

    for deployment build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.364842e89150850a1a6edccf50e1099a.js"
+  "/precache-manifest.3db9efb417e372c2544e408270eb910a.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
