0039780425cbb3234ac44f73d4d73a508687998e
mubashar-hassan-awan <mubashar.hassan@devbox.co>
2022-06-20 16:51:04 +0500

    city crud done

--- src/pages/Country/AddAndEditCountry.js
+++ src/pages/Country/AddAndEditCountry.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import { country } from "../../API/Country/CountryApis";
 
-
-export default function AddAndEditCountry({ countryState, setCountryState ,getCountries}) {
-
-
-	const notify = () => {
-		toast('Here is your toast.');
-		toast.success('Successfully created!');
-	}
+export default function AddAndEditCountry({
+  countryState,
+  setCountryState,
+  getCountries,
+}) {
+  const notify = () => {
+    toast("Here is your toast.");
+    toast.success("Successfully created!");
+  };
 
   function handleChange(evt) {
     setCountryState({
@@ -20,33 +21,44 @@ export default function AddAndEditCountry({ countryState, setCountryState ,getCo
   }
 
   const addCountry = async (params) => {
+    const loadingToastId = toast.loading("Loading..!");
     if (countryState.isAddCountry) {
       try {
-        const result = await country.addCountry(countryState.name, countryState.comments);
-				console.log(result);
-				debugger;
-				toast.success('country created!');
-				setCountryState({
-					...countryState,
-					isAddCountry:false
-				})
-				getCountries(1, "", 10);
+        const result = await country.addCountry(
+          countryState.name,
+          countryState.comments
+        );
+        if (result.error == false) {
+          toast.dismiss(loadingToastId);
+          toast.success("country created!");
+          setCountryState({
+            ...countryState,
+            isAddCountry: false,
+          });
+          getCountries(1, "", 10);
+        }
       } catch (error) {
+        toast.dismiss(loadingToastId);
         console.error(error);
       }
-    } else if(countryState.isEditCountry) {
-			try {
-				 
-        const result = await country.updateCountry(countryState.name, countryState.comments,countryState.countryId);
-				console.log(result);
-				toast.success('country updated!');
-				setCountryState({
-					...countryState,
-					isEditCountry:false
-				})
-				getCountries(1, "", 10);
-				debugger;
+    } else if (countryState.isEditCountry) {
+      try {
+        const result = await country.updateCountry(
+          countryState.name,
+          countryState.comments,
+          countryState.countryId
+        );
+        if (result.error == false) {
+          toast.dismiss(loadingToastId);
+          toast.success("country updated!");
+          setCountryState({
+            ...countryState,
+            isEditCountry: false,
+          });
+          getCountries(1, "", 10);
+        }
       } catch (error) {
+        toast.dismiss(loadingToastId);
         console.error(error);
       }
     }
@@ -60,14 +72,15 @@ export default function AddAndEditCountry({ countryState, setCountryState ,getCo
           <div className="card mb-4">
             <div className="card-body">
               <div className="card-header">
-								<h5 className="card-title">{ countryState.isAddCountry?"Add country":"Edit Country"}</h5>
+                <h5 className="card-title">
+                  {countryState.isAddCountry ? "Add country" : "Edit Country"}
+                </h5>
               </div>
-							<button onClick={notify}>Make me a toast</button>
               <Form>
                 <Form.Group controlId="formBasicName">
                   <Form.Label>Name</Form.Label>
-									<Form.Control
-										defaultValue={countryState.name}
+                  <Form.Control
+                    defaultValue={countryState.name}
                     name="name"
                     type="text"
                     placeholder="Enter Country Name"
@@ -77,8 +90,8 @@ export default function AddAndEditCountry({ countryState, setCountryState ,getCo
 
                 <Form.Group controlId="formBasicComments">
                   <Form.Label>Comments</Form.Label>
-									<Form.Control
-										defaultValue={countryState.comments}
+                  <Form.Control
+                    defaultValue={countryState.comments}
                     name="comments"
                     type="text"
                     placeholder="Comments"
@@ -103,9 +116,9 @@ export default function AddAndEditCountry({ countryState, setCountryState ,getCo
                     addCountry();
                   }}
                   // variant=`${countryState.isAddCountry?'primary':'warning'}`
-                  variant={countryState.isAddCountry?'success':'warning'}
+                  variant={countryState.isAddCountry ? "success" : "warning"}
                 >
-                   {countryState.isAddCountry?'Create':'Update'}
+                  {countryState.isAddCountry ? "Create" : "Update"}
                 </Button>
               </Form>
             </div>
