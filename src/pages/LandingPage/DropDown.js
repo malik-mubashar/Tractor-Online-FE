429bb6a5e123b287d2f9f54d9b7fcace43035a1d
AyshaAreej01 <aysha.areej@devbox.co>
2022-06-24 10:37:13 +0500

    categories on landing page

--- src/pages/LandingPage/DropDown.js
+++ src/pages/LandingPage/DropDown.js
@@ -10,39 +10,40 @@ export default function DropDown({
   autoStore,
   dropDownIcon,
   fertilizerAndSeeds,
-  plantAndHortiCulture
+  plantAndHortiCulture,
+  productHead,
 }) {
   const usedTractors = [
     {
       heading: "Search for a Used Tractor for Sale.",
       subHeading: "There are over 110k possibilities to choose from.",
       icon: "light-bulb",
-      url: "/usedtractor"
+      url: "/usedtractor",
     },
     {
       heading: "Used Tractor Featured",
       subHeading: "TractorOnline has a selection of featured tractors.",
       icon: "layers",
-      url: "/usedtractor"
+      url: "/usedtractor",
     },
     {
       heading: "Tractors for Sale",
       subHeading: "Post a free ad to swiftly sell your tractor.",
       icon: "grocery",
-      url: "/usedtractor"
+      url: "/usedtractor",
     },
     {
       heading: "Dealers of Used Tractors",
       subHeading: "Locate used tractor dealers in your area.",
       icon: "star",
-      url: "/usedtractor"
+      url: "/usedtractor",
     },
     {
       heading: "Calculator of Prices",
       subHeading: "Calculate the tractor market price.",
       icon: "recycle",
-      url: "/usedtractor"
-    }
+      url: "/usedtractor",
+    },
   ];
 
   const usedTractorsSecond = [
@@ -50,25 +51,25 @@ export default function DropDown({
       heading: "Certified Tractors on TractorOnline.com",
       subHeading:
         "Tractors that have received the TractorOnline stamp of approval",
-      icon: "speed-meter"
+      icon: "speed-meter",
     },
     {
       heading: "Tractor inspection on TractorOnline",
       subHeading: "Tractor with inspection reports from TractorOnline",
-      icon: "tick-mark"
+      icon: "tick-mark",
     },
     {
       heading: "TractorOnline sell it for me",
       subHeading:
         "Allow TractorOnline to handle the sale of your tractor for you.",
-      icon: "site-map"
+      icon: "site-map",
     },
     {
       heading: "Verification of Bid Sheet",
       subHeading:
         "For your piece of mind, we provide authentic auction sheets.",
-      icon: "data"
-    }
+      icon: "data",
+    },
   ];
 
   const autoStoreValues = [
@@ -76,19 +77,19 @@ export default function DropDown({
       heading: "Autostore TractorOnline",
       subHeading:
         "TractorOnline is your one-stop shop for auto parts and accessories.",
-      icon: "cart"
+      icon: "cart",
     },
     {
       heading: "Locate Auto Parts",
       subHeading: "Tractor parts can be found here.",
-      icon: "tick-mark"
+      icon: "tick-mark",
     },
     {
       heading: "Tractor Parts for Sale",
       subHeading:
         "Sell your tractor components instantly by placing a free ad.",
-      icon: "tag"
-    }
+      icon: "tag",
+    },
   ];
 
   const cities = [
@@ -99,7 +100,7 @@ export default function DropDown({
     "Peshawar",
     "Faisalabad",
     "Multan",
-    "Gujranwala"
+    "Gujranwala",
   ];
 
   const model = [
@@ -110,7 +111,7 @@ export default function DropDown({
     "Suzuki Mehran",
     "Toyota Vitz",
     "Suzuki Alto",
-    "Toyota Prado"
+    "Toyota Prado",
   ];
 
   const newTractors = [
@@ -118,28 +119,28 @@ export default function DropDown({
       heading: "Search for a New Tractor for Sale.",
       subHeading:
         "Over 110k possibilities are available to you to choose from.",
-      icon: "light-bulb"
+      icon: "light-bulb",
     },
     {
       heading: "New Tractor Featured",
       subHeading: "TractorOnline has some featured tractors.",
-      icon: "layers"
+      icon: "layers",
     },
     {
       heading: "Tractors for Sale",
       subHeading: "Create a free ad to sell your vehicle instantly.",
-      icon: "grocery"
+      icon: "grocery",
     },
     {
       heading: "Dealers of New Tractors",
       subHeading: "Locate new auto dealerships in your area.",
-      icon: "star"
+      icon: "star",
     },
     {
       heading: "Calculator for Prices",
       subHeading: "Determine the market value of tractors.",
-      icon: "recycle"
-    }
+      icon: "recycle",
+    },
   ];
 
   return (
@@ -151,46 +152,50 @@ export default function DropDown({
         )}
         <div
           className={`drop-down-items row py-3 ${
-            usedCars ? "usedCarsWidth" : ""
+            usedCars || productHead ? "usedCarsWidth" : ""
           } ${newCars ? "newCarsWidth" : ""} ${
             plantAndHortiCulture || fertilizerAndSeeds || autoStore
               ? "autoStoreWidth"
               : null
           }`}
         >
-          {usedCars ? (
+          {productHead ? (
             <>
               <ul className="list-unstyled col-4 p-0 border-right">
-                {usedTractors.map((option, i) => (
+                {productHead.map((item, i) => (
                   <li className="dropdown-list" key={i}>
                     <Link to="/" className="d-flex pl-1 dropdown-link">
                       <Icofont
-                        icon={option.icon}
+                        icon={item.icon}
                         height="10px"
                         width="10px"
                         className="icofont-2x ml-2 col-2 p-0"
                       />
                       <div className="col-10 p-0">
-                        <strong>{option.heading}</strong>
-                        <p>{option.subHeading}</p>
+                        <strong>{item.title}</strong>
+                        {item.product_sub_categories !== null &&
+                          item.product_sub_categories !== undefined &&
+                          item.product_sub_categories.map((y, i) => {
+                            return <li>{y.title}</li>;
+                          })}
                       </div>
                     </Link>
                   </li>
                 ))}
               </ul>
               <ul className="list-unstyled col-4 p-0 border-right">
-                {usedTractorsSecond.map((option, i) => (
+                {usedTractorsSecond.map((item, i) => (
                   <li className="dropdown-list" key={i}>
                     <Link to="/" className="d-flex pl-1 dropdown-link">
                       <Icofont
-                        icon={option.icon}
+                        icon={item.icon}
                         height="10px"
                         width="10px"
                         className="icofont-2x ml-2 col-2 p-0"
                       />
                       <div className="col-10 p-0">
-                        <strong>{option.heading}</strong>
-                        <p>{option.subHeading}</p>
+                        <strong>{item.heading}</strong>
+                        <p>{item.subHeading}</p>
                       </div>
                     </Link>
                   </li>
@@ -203,14 +208,14 @@ export default function DropDown({
                     <strong>Popular Cities</strong>
                   </Link>
                 </li>
-                {cities.map((option, i) => (
+                {cities.map((item, i) => (
                   <li key={i}>
                     <Link
                       to="/"
                       className="dropdown-link"
                       title="Used Cars for sale in Karachi"
                     >
-                      <p className="city-name">{option}</p>
+                      <p className="city-name">{item}</p>
                     </Link>
                   </li>
                 ))}
@@ -222,123 +227,14 @@ export default function DropDown({
                     <strong>Popular Models</strong>
                   </Link>
                 </li>
-                {model.map((option, i) => (
+                {model.map((item, i) => (
                   <li key={i}>
                     <Link
                       to="/"
                       className="dropdown-link"
                       title="Used Cars for sale in Karachi"
                     >
-                      <p className="city-name">{option}</p>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) : null}
-
-          {newCars ? (
-            <>
-              <ul className="list-unstyled col-6 border-right">
-                {newTractors.map((option, i) => (
-                  <li className="dropdown-list" key={i}>
-                    <Link to="/" className="d-flex pl-1 dropdown-link">
-                      <Icofont
-                        icon={option.icon}
-                        height="10px"
-                        width="10px"
-                        className="icofont-2x ml-2 col-2 p-0"
-                      />
-                      <div className="col-10 p-0">
-                        <strong>{option.heading}</strong>
-                        <p>{option.subHeading}</p>
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <ul className="list-unstyled col-6  text-center">
-                <li className="mb-3">
-                  <Link to="/">
-                    <Icon.MapPin className="icon mr-2" />
-                    <strong>Popular New Tractor</strong>
-                  </Link>
-                </li>
-                {model.map((option, i) => (
-                  <li key={i}>
-                    <Link
-                      to="/"
-                      className="dropdown-link"
-                      title="Used Cars for sale in Karachi"
-                    >
-                      <p className="city-name">{option}</p>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) : null}
-
-          {autoStore ? (
-            <>
-              <ul className="list-unstyled col-12 ">
-                {autoStoreValues.map((option, i) => (
-                  <li className="dropdown-list" key={i}>
-                    <Link to="/" className="d-flex pl-1 dropdown-link">
-                      <Icofont
-                        icon={option.icon}
-                        height="10px"
-                        width="10px"
-                        className="icofont-2x ml-2 col-2 p-0"
-                      />
-                      <div className="col-10 p-0">
-                        <strong>{option.heading}</strong>
-                        <p>{option.subHeading}</p>
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) : null}
-          {fertilizerAndSeeds ? (
-            <>
-              <ul className="list-unstyled col-12 ">
-                {autoStoreValues.map((option, i) => (
-                  <li className="dropdown-list" key={i}>
-                    <Link to="/" className="d-flex pl-1 dropdown-link">
-                      <Icofont
-                        icon={option.icon}
-                        height="10px"
-                        width="10px"
-                        className="icofont-2x ml-2 col-2 p-0"
-                      />
-                      <div className="col-10 p-0">
-                        <strong>{option.heading}</strong>
-                        <p>{option.subHeading}</p>
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) : null}
-          {plantAndHortiCulture ? (
-            <>
-              <ul className="list-unstyled col-12 ">
-                {autoStoreValues.map((option, i) => (
-                  <li className="dropdown-list" key={i}>
-                    <Link to="/" className="d-flex pl-1 dropdown-link">
-                      <Icofont
-                        icon={option.icon}
-                        height="10px"
-                        width="10px"
-                        className="icofont-2x ml-2 col-2 p-0"
-                      />
-                      <div className="col-10 p-0">
-                        <strong>{option.heading}</strong>
-                        <p>{option.subHeading}</p>
-                      </div>
+                      <p className="city-name">{item}</p>
                     </Link>
                   </li>
                 ))}
