fd45127d3921fcdedc369f302cf54e5d21106315
mubashar-hassan-awan <mubashar.hassan@devbox.co>
2022-12-01 17:34:20 +0500

    1 dec build post and add fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4d1cb63e95a0510dbe20e74bd177148.js"
+  "/precache-manifest.7c7a18f763033686ed65597389803436.js"
 );
 
 workbox.clientsClaim();
